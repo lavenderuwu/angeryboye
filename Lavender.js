0f1e5b7e5402f8f1b7c26c46991ab171e1d400cc
unknown <lvelavender@gmail.com>
2020-04-29 00:14:56 +0200

    first commit

---
+++ Lavender.js
@@ -0,0 +1,27 @@
+alert("do you speak uguu ?")
+
+let lve = prompt("uguuuu? - yes or no")
+
+if (lve == "yes") {
+    
+    let name = prompt("what is your name uwu ?")
+    alert(`welcome ${name}`)
+
+
+}
+if (lve == "no" ) {
+    alert(`angeryboye `)
+}
+
+if (lve == "YES") {
+    alert("PEPOMAD WHY ARE YOU SHOUTING")
+    
+}
+
+
+
+
+ else {
+    alert("I CAN ONLY UNDERSTAND UGUU")
+
+}
